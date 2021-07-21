9909d73378fb4166e69df8dfaa038709037bd48a
Egor Zyuskin <ezyuskin@utrace.ru>
2021-07-21 23:35:23 +0600

    fix: chnage alpha.demo.nbki.ru test url

--- build_and_test.sh
+++ build_and_test.sh
@@ -3,6 +3,6 @@
 docker build -t openssl-gost-local .
 
 docker run -it --rm openssl-gost-local bash -c "openssl ciphers | grep 'GOST2012-GOST8912-GOST8912' && echo OK || echo FAIL"
-docker run -it --rm openssl-gost-local curl https://alpha.demo.nbki.ru -k && echo "OK" || echo "FAIL"
+docker run -it --rm openssl-gost-local curl https://alpha.demo.nbki.ru/main -k && echo "OK" || echo "FAIL"
 docker run -it --rm openssl-gost-local curl https://zakupki.gov.ru -k && echo "OK" || echo "FAIL"
 docker run -it --rm openssl-gost-local curl https://portal.rosreestr.ru:4455 -k && echo "OK" || echo "FAIL"
